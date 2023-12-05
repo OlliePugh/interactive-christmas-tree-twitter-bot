e80c2bcfad1963dfa2997bada33cb6c0f60be2ef
Ollie Pugh <oliver.pugh@mac.com>
2023-12-05 18:55:55 +0000

    init commit

---
+++ src/index.ts
@@ -0,0 +1,96 @@
+import takeScreenshot from "./take-screenshot";
+import { TwitterApi, EUploadMimeType } from "twitter-api-v2";
+import axios from "axios";
+import Jimp from "jimp";
+import "dotenv/config";
+
+const formatDateTime = (date: Date) => {
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  const ordinalSuffix = (day: number) => {
+    if (day >= 11 && day <= 13) {
+      return "th";
+    }
+    switch (day % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  };
+
+  const formattedDate = `${day}${ordinalSuffix(day)} of ${month} ${year}`;
+  const formattedTime = `${hours}:${minutes}`;
+
+  return `${formattedDate} at ${formattedTime}`;
+};
+console.log(process.env.TWITTER_ACCESS_TOKEN);
+// TODO:  re add all items with a ts ignore?
+const client = new TwitterApi({
+  //@ts-ignore
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_SECRET,
+  bearerToken: process.env.TWITTER_BEARER,
+});
+const baubleBuffer = async (id: number) => {
+  console.log(`getting bauble ${id}`);
+  const result = await axios.get(
+    `https://europe-west1-real-world-games.cloudfunctions.net/getBaubleBmp?id=${id}`,
+    {
+      responseType: "arraybuffer",
+    }
+  );
+  console.log(`downloaded bauble ${id}`);
+
+  const image = await Jimp.read(result.data);
+  const resized = image.resize(640, 480);
+  const png = resized.getBufferAsync(Jimp.MIME_PNG);
+
+  console.log("converted to png");
+  return png;
+};
+
+const addMedia = async (buffer: Buffer): Promise<string> => {
+  const mediaId = await client.v1.uploadMedia(buffer, {
+    mimeType: EUploadMimeType.Png,
+  });
+  console.log(`uploaded media with id ${mediaId}`);
+  return mediaId;
+};
+
+const postTweet = async () => {
+  const imagePromises = [
+    takeScreenshot(),
+    baubleBuffer(1),
+    baubleBuffer(2),
+    baubleBuffer(3),
+  ];
+
+  const uploadPromises = (await Promise.all(imagePromises)).map((x) =>
+    addMedia(x)
+  );
+
+  const mediaIds = await Promise.all(uploadPromises);
+
+  console.log(`media ids: ${mediaIds}`);
+
+  await client.v2.tweet({
+    text: `Current status of the Interactive Christmas Tree as of the ${formatDateTime(
+      new Date()
+    )}\n\nControl the tree yourself at interactive-christmas-tree.com\n\n#interactivechristmastree #christmas #interactive #christmastree`,
+    media: { media_ids: mediaIds as string[] },
+  });
+  console.log("done");
+};
+
+postTweet();
---
+++ src/take-screenshot.ts
@@ -0,0 +1,41 @@
+import puppeteer from "puppeteer";
+
+const urlToOpen = "https://tree.ollieq.co.uk"; // Replace with the URL you want to open
+
+const takeScreenshot = async (): Promise<Buffer> => {
+  console.log("launch puppeteer");
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  await page.setViewport({ width: 1200, height: 2000 });
+  await page.goto(urlToOpen);
+  console.log("load page");
+
+  // Wait for the video element to be present
+  await page.waitForSelector("video");
+
+  // Wait for 10 seconds (adjust as needed)
+  console.log("waiting for video");
+  await page.waitForTimeout(10000);
+
+  // Click the button with the id "hide-overlay-button"
+  await page.click("#hide-overlay-button");
+
+  // Wait for the overlay to hide (assuming it triggers some asynchronous action)
+  // You might need to adjust this waiting logic based on your specific scenario
+  await page.waitForTimeout(100); // Adjust the time as needed
+
+  // Capture a screenshot of the video element
+  const videoElement = await page.$("video");
+  if (!videoElement) {
+    throw "Video element not found";
+  }
+  console.log("take screenshot");
+  const screenshotBuffer = await videoElement.screenshot();
+  await browser.close();
+  console.log("returning buffer");
+  return screenshotBuffer;
+  await browser.close();
+};
+
+export default takeScreenshot;
